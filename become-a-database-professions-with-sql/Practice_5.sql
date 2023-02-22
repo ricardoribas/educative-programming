5cee592ab9de9650238f39a5262568d09620404b
ricardoribas <ricardo@dodo.eco>
2023-02-22 13:26:53 +0000

    WIP with the last practice sessions

---
+++ become-a-database-professions-with-sql/Practice_5.sql
@@ -0,0 +1,13 @@
+-- PRACTICE SET 5
+-- https://www.educative.io/module/page/JZmo10C1K3V0gqV0w/10370001/5844917766586368/5026739381600256
+
+-- Exercise 1: Calculate the running total of the revenue generated per week 
+-- for the first 10 weeks for the movie Avengers.
+
+-- Exercise 2: Calculate the total revenue of each Genre and find the percentage of revenue of each
+
+-- Exercise 3: Calculate the moving average of revenue generated in a three week window for the movie Ocean’s 11
+
+-- Exercise 4: Find the value of RevenueInMillions at the start of each month for the movie Mr. & Mrs. Smith
+
+-- Exercise 5: Calculate the monthly growth rate of revenue for the movie Mission Impossible
---
+++ become-a-database-professions-with-sql/Practice_6.sql
@@ -0,0 +1,14 @@
+-- PRACTICE SET 6
+-- https://www.educative.io/module/page/JZmo10C1K3V0gqV0w/10370001/5844917766586368/6298497074069504
+
+-- Exercise 1: Find the top two movies of distributors who have more than one movie to their name
+
+-- Exercise 2: Find the total, average, minimum and maximum of the production
+-- budget and revenue earned from the Movies table and append the summary data to the top of the table.
+
+-- Exercise 3: Calculate the mean median and mode of the running time of movies
+
+-- Exercise 4: Find the correlation between budget, collection 
+-- and running time of movies and display the results as a table.
+
+-- Exercise 5: Find the market share of top 3 distributors and aggregate the remaining distributors in a single row.
